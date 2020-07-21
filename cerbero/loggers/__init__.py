a9960c111b02b1b71be2ce30338f9c34b5638023
Francisco Gonzalez <francisco.gonzalez@strong.io>
2020-07-21 15:02:11 -0500

    Expose logger classes

---
+++ cerbero/loggers/__init__.py
@@ -0,0 +1,4 @@
+from .checkpointer import Checkpointer, CheckpointerConfig
+from .log_manager import LogManager, LogManagerConfig
+from .log_writer import LogWriter, LogWriterConfig
+from .tensorboard_writer import TensorBoardWriter
